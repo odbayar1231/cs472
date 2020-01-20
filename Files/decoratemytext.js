86cb084bcda85b52c124fe427b9ae4002594ef10
Odbayar <57197505+odbayar1231@users.noreply.github.com>
2020-01-20 07:06:57 -0600

    addded

--- Files/decoratemytext.js
+++ Files/decoratemytext.js
@@ -2,12 +2,13 @@
 (function(){
 
     
-    
 }) ();
 
 window.onload = function () {
 
     var decorateButton = document.getElementById('decButton');
+    var PigLaitButton = document.getElementById('PigLaitButton');
+    var MalkovitchButton = document.getElementById('Malkovitch');
     var textArea = document.getElementById('decTextArea');
     var chBox = document.getElementById('chBox');
 
@@ -20,6 +21,7 @@ window.onload = function () {
     }
 
     decorateButton.onclick = function () {
+	alert("Hello, world!");
         setInterval(increaseFontSize, 2000);
     }
 
@@ -28,7 +30,7 @@ window.onload = function () {
             textArea.style.fontWeight = 'bold';
             textArea.style.color = 'green';
             textArea.style.textDecoration = 'underline';
-            document.getElementById('body').style.backgroundImage = "url('https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_face:auto,h_494,q_60,w_1700/v1/clients/saltlake/SkiCity_ResortsMarch2017_HiRez_AustenDiamondPhotography_69_f2359e3f-0f6c-430d-8392-7146a18714b6.jpg')";
+            document.getElementById('body').style.backgroundImage = "url('https://courses.cs.washington.edu/courses/cse190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
         }
         else {
             textArea.style.fontWeight = 'normal';
@@ -36,6 +38,40 @@ window.onload = function () {
             textArea.style.textDecoration = 'none';
             document.getElementById('body').style.backgroundImage = 'none';
         }
+    }
+
+        PigLaitButton.onclick = function () {
+        let words = textArea.value.split(' ');
+        let newWords = '';
+        words.forEach(function (element) {
+            if (isVowel(element.charAt(0))) {
+                element = element + 'ay'
+                newWords += element + ' ';
+            }
+            else {
+                element = element.substr(1, element.lenght) + element.charAt(0) + 'ay'
+                newWords += element + ' ';
+            }
+        });
+        textArea.value = newWords;
+    }
+
+    MalkovitchButton.onclick = function () {
+        let words = textArea.value.split(' ');
+        let newWords = ''
+        words.forEach(element => {
+            if (element.length >= 5)
+                element = 'Malkovich';
+
+            newWords += element + ' ';
+        });
+        textArea.value = newWords;
+    }
+
+    function isVowel(x) {
+        let result;
+        result = x.toLowerCase() == "a" || x.toLowerCase() == "e" || x.toLowerCase() == "i" || x.toLowerCase() == "o" || x.toLowerCase() == "u";
+        return result;
     }
 
 }
